e574208dac642223e1d332e4b6d325f0d27b3f7b
darg <darg20127@gmail.com>
2017-03-15 01:32:05 +0800

    set production mode

--- webpack.config.js
+++ webpack.config.js
@@ -39,9 +39,8 @@ module.exports = {
 	},
 	plugins: [
 		new webpack.DefinePlugin({
-			'process.env': {
-				'NODE_ENV': JSON.stringify('development')
-			}
-		})
+			'process.env.NODE_ENV': JSON.stringify('production')
+		}),
+		new webpack.optimize.UglifyJsPlugin()
 	]
 };
