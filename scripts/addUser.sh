2639e824f72f475d0b06a504e11006da8e1f977b
tsmh <darg20127@gmail.com>
2017-09-13 00:10:41 +0800

    lazy man

--- scripts/addUser.sh
+++ scripts/addUser.sh
@@ -3,6 +3,7 @@
 users=("darg" "test")
 for user in "${users[@]}"; do
   mkdir -p Build/Server/Home/$user/{scheduler,simulator,platform,generator}
+  mkdir -p Server/Home/$user/{scheduler,simulator,platform,generator}
   node Build/Server/User/CreateUser.js "$user"
 done
 
--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+
 ./node_modules/webpack/bin/webpack.js --progress --config webpack.prod.js
 ./node_modules/babel-cli/bin/babel.js -D -d Build/Server Server
+
+curl -o Build/Server/Secrets/TLS/cert.pem --create-dirs https://gist.githubusercontent.com/darg20127/03709547d03161d68a6a9ba19872af86/raw/cert.pem
+curl -o Build/Server/Secrets/TLS/key.pem.pem --create-dirs https://gist.githubusercontent.com/darg20127/03709547d03161d68a6a9ba19872af86/raw/key.pem
+curl -o Build/Server/Secrets/Config.json --create-dirs https://gist.githubusercontent.com/darg20127/03709547d03161d68a6a9ba19872af86/raw/Config.json
