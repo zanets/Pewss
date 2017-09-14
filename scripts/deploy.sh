d2fb234cc8bacb50c105c3936387b0fad58b4ef1
tsmh <darg20127@gmail.com>
2017-09-14 11:31:27 +0800

    dynamic create pewss.default

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cp contrib/init/debian/pewss /etc/init.d/pewss
-cp contrib/init/debian/pewss.default /etc/default/pewss
+echo "APP_DIR=$(pwd)\nNODE=$(which node)\n" > /etc/default/pewss
 
 update-rc.d pewss defaults 
