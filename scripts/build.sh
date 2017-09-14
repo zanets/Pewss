08ea728376c5f39543260b893734da5388a9289e
tsmh <darg20127@gmail.com>
2017-09-14 21:18:01 +0800

    typo

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,6 +4,6 @@
 ./node_modules/babel-cli/bin/babel.js -D -d Build/Server Server
 
 curl -o Build/Server/Secrets/TLS/cert.pem --create-dirs https://gist.githubusercontent.com/darg20127/03709547d03161d68a6a9ba19872af86/raw/cert.pem
-curl -o Build/Server/Secrets/TLS/key.pem.pem --create-dirs https://gist.githubusercontent.com/darg20127/03709547d03161d68a6a9ba19872af86/raw/key.pem
+curl -o Build/Server/Secrets/TLS/key.pem --create-dirs https://gist.githubusercontent.com/darg20127/03709547d03161d68a6a9ba19872af86/raw/key.pem
 curl -o Build/Server/Secrets/Config.json --create-dirs https://gist.githubusercontent.com/darg20127/03709547d03161d68a6a9ba19872af86/raw/Config.json
 
